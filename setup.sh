7f6b6936cac607e936e3cb22793e6868ae5c0936
Alex Fall <alex@fallalex.com>
2021-09-03 23:00:36 -0400

    vim-plug install

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ cp git/gitconfig ~/.gitconfig
 
 mkdir -p ~/.config/nvim/
 ln -sf `realpath vim/init.vim` ~/.config/nvim/
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # run :PlugInstall
 
 mkdir -p ~/.config/zsh/
